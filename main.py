f02cc284dab015c49b8f80e879aba887851aa807
Luby <44274944+RealLubY@users.noreply.github.com>
2021-02-06 15:22:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,23 @@ async def on_ready():
     channel = int(input("Channelid\n"))
     timelist = []
     async for message in bot.get_channel(channel).history(limit=None):
+        if message == None:
+            continue
         if message.type == discord.MessageType.call:
+            print(f"Call: {message.call.duration} - Secs: {message.call.duration.total_seconds()} - ID: {message.id}")
             timelist.append(message.call.duration.total_seconds())
     total_time = 0
     for i in timelist:
         total_time += i
+    print("-----------")
     print(f"{str(datetime.timedelta(seconds=total_time))}")
+    print("-----------")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, discord.ext.commands.CommandNotFound):
+        return
+    raise error
+
 
 bot.run("", bot=False) # TOKEN
